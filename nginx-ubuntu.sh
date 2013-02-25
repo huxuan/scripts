ffb9a0f01773df563dc4392f5659cc0d3126adca
huxuan <i@huxuan.org>
2013-02-25 16:11:18 +0000

    nginx-ubuntu: use ppa instead of raw soure list

--- nginx-ubuntu.sh
+++ nginx-ubuntu.sh
@@ -4,7 +4,7 @@
 # | Author: huxuan
 # | E-mail: i(at)huxuan.org
 # | Created: 2013-02-25
-# | Last modified: 2013-02-25
+# | Last modified: 2013-02-26
 # | Description:
 # |     Nginx installation for Ubuntu
 # |
@@ -12,17 +12,8 @@
 # | License GPLv3
 # +-----------------------------------------------------------------------------
 
-# Get Ubuntu release codename
-CODENAME=$(lsb_release -sc)
-
-# Add apt key
-wget -q http://nginx.org/keys/nginx_signing.key -O- | sudo apt-key add -
-
-# Add source list
-sudo tee /etc/apt/sources.list.d/nginx.list << EOF
-deb http://nginx.org/packages/ubuntu/ $CODENAME nginx
-deb-src http://nginx.org/packages/ubuntu/ $CODENAME nginx
-EOF
+# add ppa
+sudo add-apt-repository ppa:nginx/stable
 
 # update and install
 sudo apt-get update
